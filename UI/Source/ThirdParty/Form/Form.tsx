15bf18d97f375daddf70080d37b16228a8d18d37
Luke Briggs <luke@kulestar.com>
2025-03-14 16:03:22 +0000

    Form refurb

---
+++ UI/Source/ThirdParty/Form/Form.tsx
@@ -0,0 +1,148 @@
+import submitForm from 'UI/Functions/SubmitForm';
+import Spacer from 'UI/Spacer';
+import Alert from 'UI/Alert';
+import Loading from 'UI/Loading';
+import Input from 'UI/Input';
+import { useState, useEffect, useRef } from 'react'; 
+
+/**
+ * Props for the Form component.
+ */
+interface FormProps<ResponseType, FieldType> extends React.HTMLAttributes<HTMLFormElement> {
+	action: (fields: FieldType) => Promise<ResponseType>,
+	resetOnSubmit?:boolean,
+	failedMessage?: React.ReactNode,
+	loadingMessage?: string,
+	successMessage?: React.ReactNode,
+	submitEnabled?: boolean,
+	submitLabel?: string,
+	onSuccess?: (response: ResponseType) => void,
+	onFailed?: (e: PublicError) => void,
+	onValues?: (values: FieldType) => FieldType,
+	onSubmitted?: (values: FieldType) => void
+}
+
+/**
+ * Wraps <form> in order to automatically manage setting up default values.
+ * You can also directly use form and the Functions/SubmitForm method if you want - use of this component is optional.
+ * This component is best used with UI/Input.
+ */
+const Form = <ResponseType extends any, FieldType extends any>(props: FormProps<ResponseType, FieldType>) => {
+	const {
+		action,
+		children,
+		failedMessage,
+		loadingMessage,
+		successMessage,
+		submitEnabled,
+		submitLabel,
+		resetOnSubmit,
+		onSubmitted,
+		onSuccess,
+		onFailed,
+		onValues,
+		...attribs
+	} = props;
+
+	const formRef = useRef<HTMLFormElement>(null);
+	const [loading, setLoading] = useState(false);
+	const [success, setSuccess] = useState(false);
+	const [failed, setFailed] = useState <PublicError | null>(null);
+
+	useEffect(() => {
+		setFailed(null);
+		setSuccess(false);
+		setLoading(false);
+	}, [action]);
+
+	const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		setLoading(true);
+
+		submitForm(e)
+			.then((rawVals: Record<string, any>) => {
+				var values = rawVals as FieldType;
+				values = onValues ? onValues(values) : values;
+				onSubmitted && onSubmitted(values);
+				return action(values);
+			})
+			.then((r: ResponseType) => {
+				if (resetOnSubmit) {
+					formRef.current?.reset();
+				}
+
+				setLoading(false);
+				setFailed(null);
+				setSuccess(true);
+				onSuccess && onSuccess(r);
+			})
+			.catch((e : any) => {
+				console.error(e);
+
+				var error: PublicError = (e?.message) ? e as PublicError : {
+					type: 'validation',
+					message: `Unable to send this form - please check your answers`,
+					detail: e
+				};
+
+				setLoading(false);
+				setFailed(error);
+				setSuccess(false);
+				onFailed && onFailed(e);
+			});
+
+		return false;
+	};
+	
+	var showFormResponse = !!(loadingMessage || submitLabel || failedMessage);
+	var submitDisabled = loading || (submitEnabled !== undefined && submitEnabled != true);
+	
+	return (
+		<form
+			onSubmit={onSubmit}
+			ref={formRef}
+			method={"post"}
+			{...attribs}
+		>
+			{children}
+			{showFormResponse && (
+				<div className="form-response">
+					<Spacer />
+					{
+						failed && failedMessage && (
+							<div className="form-failed">
+								<Alert variant="danger">
+									{failedMessage}
+								</Alert>
+								<Spacer />
+							</div>
+						)
+					}
+					{
+						success && successMessage && (
+							<div className="form-success">
+								<Alert variant="success">
+									{successMessage}
+								</Alert>
+								<Spacer />
+							</div>
+						)
+					}
+					{
+						submitLabel && <Input type="submit" label={submitLabel} disabled={submitDisabled} />
+					}
+					{
+						loading && loadingMessage && (
+							<div className="form-loading">
+								<Spacer />
+								<Loading message={loadingMessage}/>
+							</div>
+						)
+					}
+				</div>
+			)}
+		</form>
+	);
+	
+}
+
+export default Form;
---
+++ UI/Source/ThirdParty/Functions/SubmitForm/SubmitForm.tsx
@@ -0,0 +1,78 @@
+/**
+ * Used to construct a form field set and returns it in a promise.
+ * If the fields were unable to be constructed because they contain validation errors 
+ * then the promise will reject with a PublicError stating the validation failure itself.
+ * @param e
+ * @returns A promise which is rejected if any of the fields have validation errors.
+ */
+const SubmitForm = (e: React.FormEvent<HTMLFormElement>) => {
+	e.preventDefault();
+	
+	var fields = (e.target as HTMLFormElement)?.elements;
+	var values: Record<string, any> = {};
+	var valuePromises = [];
+	
+	for (var i = 0; i < fields.length; i++) {
+		valuePromises.push(collectFieldValue(fields[i], values));
+	}
+
+	var submitterName: string = (e as any).submitter?.name;
+
+	if(submitterName){
+		values[submitterName] = (e as any).submitter.value;
+	}
+	
+	return Promise.all(valuePromises)
+		.then(() => values);
+}
+
+/**
+ * Collects the given form field's value, putting it in to the given values set.
+ * Handles a custom field function called onGetValue such that a form can contain types beyond strings only.
+ * @param field
+ * @param values
+ * @returns A promise which is rejected if any of the fields have validation errors.
+ */
+const collectFieldValue = (field: any, values: Record<string, any>) : Promise<void> => {
+	return new Promise((success, reject) => {
+		if (!field.name || field.type == 'submit' || (field.type == 'radio' && !field.checked)) {
+			// Not a field we care about.
+			success();
+			return;
+		}
+
+		var name: string = field.name as string;
+
+		if (field.onValidationCheck) {
+			var valError = field.onValidationCheck(field) as PublicError;
+
+			if (valError) {
+				reject(valError);
+				return;
+			}
+		}
+
+		var value = field.type == 'checkbox' ? field.checked : field.value;
+
+		if (field.onGetValue) {
+			value = field.onGetValue(value, field);
+
+			if (value && value.then && typeof value.then === 'function') {
+				// It's a promise.
+				// Must wait for all of these before proceeding.
+				value.then((val: string) => {
+					field.value = val;
+					values[name] = val;
+					success();
+				});
+			} else {
+				field.value = value;
+			}
+		}
+
+		values[name] = value;
+		success();
+	});
+}
+
+export default SubmitForm;
