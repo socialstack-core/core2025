f52d3d2294846b57bd9d042a9555a0a884255a11
hudson1998x <jhudsonjejames@gmail.com>
2025-03-17 13:25:31 +0000

    Added small amendment to allow a choice in the side the sidebar is positioned.

--- UI/Source/BaseWebTemplate/BaseWebTemplate.tsx
+++ UI/Source/BaseWebTemplate/BaseWebTemplate.tsx
@@ -3,6 +3,7 @@ interface SidebarTemplateProps {
     body?: React.ReactNode,
     sidebar?: React.ReactNode,
     footer?: React.ReactNode,
+    sidebarSide?: 'left' | 'right'
 }
 
 export default (props : SidebarTemplateProps) => {
@@ -11,8 +12,9 @@ export default (props : SidebarTemplateProps) => {
           <div id="wrapper">
                {props.header && <header>{props.header}</header>}
                <div id="content">
+                    {props.sidebar && props.sidebarSide == 'left' && <aside>{props.sidebar}</aside>}
                     {props.body && <main>{props.body}</main>}
-                    {props.sidebar && <aside>{props.sidebar}</aside>}
+                    {props.sidebar && props.sidebarSide == 'right' && <aside>{props.sidebar}</aside>}
                </div>
                {props.footer && <footer>{props.footer}</footer>}
           </div>
