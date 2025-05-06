205c63b6e99c9cbcbc233e769c094a853e493275
hudson1998x <jhudsonjejames@gmail.com>
2025-05-06 12:47:18 +0100

    [Templates] Created components for canvas, slot and wrapper.

---
+++ Admin/Source/ThirdParty/Template/Slot/Slot.tsx
@@ -0,0 +1,7 @@
+
+
+const Slot: React.FC<React.PropsWithChildren> = (props: React.PropsWithChildren): React.ReactNode => {
+    return props.children;
+}
+
+export default Slot;
---
+++ Admin/Source/ThirdParty/Template/Wrapper/Wrapper.tsx
@@ -0,0 +1,7 @@
+
+
+const Wrapper: React.FC<React.PropsWithChildren> = (props: React.PropsWithChildren): React.ReactNode => {
+    return props.children;
+}
+
+export default Wrapper;
