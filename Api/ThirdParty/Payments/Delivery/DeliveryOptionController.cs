03347127d358d86b144dd69cf92cca5f417a897f
Luke Briggs <luke@kulestar.com>
2025-05-22 14:01:39 +0100

    Exposed the delivery estimation as an endpoint

--- Api/ThirdParty/Payments/Delivery/DeliveryOptionController.cs
+++ Api/ThirdParty/Payments/Delivery/DeliveryOptionController.cs
@@ -1,4 +1,7 @@
+using Api.Contexts;
+using Api.Startup;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace Api.Payments;
 
@@ -6,4 +9,37 @@ namespace Api.Payments;
 [Route("v1/deliveryoption")]
 public partial class DeliveryOptionController : AutoController<DeliveryOption>
 {
+	private DeliveryService _deliveries;
+	private DeliveryOptionService _options;
+	private ShoppingCartService _carts;
+
+	/// <summary>
+	/// Instanced automatically (singleton).
+	/// </summary>
+	/// <param name="deliveries"></param>
+	/// <param name="carts"></param>
+	/// <param name="options"></param>
+	public DeliveryOptionController(DeliveryService deliveries, ShoppingCartService carts, DeliveryOptionService options)
+	{
+		_deliveries = deliveries;
+		_carts = carts;
+		_options = options;
+	}
+
+	/// <summary>
+	/// Estimate the given shopping cart. The contextual user must be able to load the cart of course!
+	/// If estimates are already generated, this will return the existing set.
+	/// </summary>
+	/// <param name="context"></param>
+	/// <param name="shoppingCartId"></param>
+	/// <returns></returns>
+	[HttpGet("estimate/cart/{shoppingCartId}")]
+	public async ValueTask<ContentStream<DeliveryOption, uint>> Estimate(Context context, [FromRoute] uint shoppingCartId)
+	{
+		var cart = await _carts.Get(context, shoppingCartId);
+		var estimates = await _deliveries.EstimateDelivery(context, cart);
+
+		return new ContentStream<DeliveryOption, uint>(estimates, _options);
+	}
+
 }
--- Api/ThirdParty/Payments/Delivery/DeliveryService.cs
+++ Api/ThirdParty/Payments/Delivery/DeliveryService.cs
@@ -6,6 +6,7 @@ using Api.Contexts;
 using Api.Eventing;
 using Api.PasswordResetRequests;
 using System;
+using Api.Startup;
 
 namespace Api.Payments
 {
@@ -16,19 +17,39 @@ namespace Api.Payments
 	public partial class DeliveryService : AutoService
     {
 		private DeliveryOptionService _options;
+		private AddressService _addresses;
 
 		/// <summary>
 		/// Instanced automatically. Use injection to use this service, or Startup.Services.Get.
 		/// </summary>
-		public DeliveryService(DeliveryOptionService options)
+		public DeliveryService(DeliveryOptionService options, AddressService addresses)
         {
 			_options = options;
+			_addresses = addresses;
 		}
 
 		/// <summary>
 		/// Collects a potentially cached set of delivery estimates for the given shopping cart.
 		/// </summary>
+		/// <param name="context"></param>
+		/// <param name="cart"></param>
+		/// <returns></returns>
 		public async ValueTask<List<DeliveryOption>> EstimateDelivery(Context context, ShoppingCart cart)
+		{
+			var address = await _addresses.Get(context, cart.AddressId, DataOptions.IgnorePermissions);
+
+			if (address == null)
+			{
+				throw new PublicException("A target delivery address has not been set.", "delivery/no_address");
+			}
+
+			return await EstimateDelivery(context, cart, address);
+		}
+
+		/// <summary>
+		/// Collects a potentially cached set of delivery estimates for the given shopping cart and target address.
+		/// </summary>
+		private async ValueTask<List<DeliveryOption>> EstimateDelivery(Context context, ShoppingCart cart, Address targetAddress)
 		{
 			// Get the current estimate set for this cart:
 			var currentOpts = await _options
@@ -46,7 +67,7 @@ namespace Api.Payments
 
 				foreach (var item in currentOpts)
 				{
-					if (item.CreatedUtc < lastModified || item.CreatedUtc < anHourAgo)
+					if (item.CreatedUtc < lastModified || item.CreatedUtc < anHourAgo || item.AddressId != targetAddress.Id)
 					{
 						// It's older than the cart is, or more than an hour old. Regen them all.
 						stale = true;
@@ -71,7 +92,8 @@ namespace Api.Payments
 
 			// Collect the estimates now. This is very site specific so you'll need to create a handler for this event.
 			var estimate = await Events.DeliveryOption.Estimate.Dispatch(context, new DeliveryEstimates() {
-				Cart = cart
+				Cart = cart,
+				Target = targetAddress
 			});
 
 			if (estimate.Options == null)
@@ -88,6 +110,7 @@ namespace Api.Payments
 				var estimateJson = Newtonsoft.Json.JsonConvert.SerializeObject(item);
 				var opt = await _options.Create(context, new DeliveryOption()
 				{
+					AddressId = targetAddress.Id,
 					ShoppingCartId = cart.Id,
 					InformationJson = estimateJson
 				}, DataOptions.IgnorePermissions);
