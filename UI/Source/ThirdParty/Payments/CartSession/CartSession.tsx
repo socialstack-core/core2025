5227645fd6f6c9e8e59ef9fec15ab7ef08550313
Luke Briggs <luke@kulestar.com>
2025-05-27 11:32:39 +0100

    CartSession mostly converted (waiting on some typescript includes fixes) and now uses the new endpoint/ server stored cart

---
+++ UI/Source/ThirdParty/Payments/CartSession/CartSession.tsx
@@ -0,0 +1,118 @@
+import shoppingCartApi, { ShoppingCart } from 'Api/ShoppingCart';
+import { createContext, useContext, useState, useEffect } from 'react';
+// import { useCart } from 'UI/Payments/CartSession';
+// var { addToCart, emptyCart, shoppingCart } = useCart();
+// addToCart({product: ProductIdOrObject, quantity: PositiveOrNegativeNumber});
+// addToCart({product: ProductIdOrObject, isSubscribing: true}); (adds a quantity of 1)
+// To remove either, just addToCart with a negative quantity.
+import store from 'UI/Functions/Store';
+
+interface CartContext {
+}
+
+const CartSession = createContext<CartContext>({
+    
+});
+
+export const Provider: React.FC<React.PropsWithChildren> = (props) => {
+    const [shoppingCart, setShoppingCart] = useState<ShoppingCart | null>(null);
+    const [loading, setLoading] = useState<boolean>(true);
+
+    const includeSet = [
+        shoppingCartApi.includes.productquantities,
+        'productquantities.product',
+        'productquantities.product.price',
+    ];
+
+    useEffect(() => {
+        var cartId = store.get('shopping_cart_id') as string;
+        if (cartId) {
+            shoppingCartApi.load(parseInt(cartId) as int, includeSet)
+                .then(response => {
+                    setShoppingCart(response);
+                    setLoading(false);
+                }).catch(e => {
+                    console.error(e);
+                    setLoading(false);
+                })
+        } else {
+            setLoading(false);
+        }
+    }, []);
+
+    // return quantity of given product within cart (or all products if productId == null)
+    let getCartQuantity = (productId: uint) => {
+        var qty = 0;
+
+        shoppingCart?.productQuantities?.forEach(product => {
+            if (productId == product?.productId) {
+                qty += product?.quantity || 0;
+            }
+        });
+
+        return qty;
+    }
+
+    let hasSubscriptions = () => {
+        var result = false;
+
+        shoppingCart?.productQuantities?.forEach(productQty => {
+            var product = productQty?.product;
+            if (product?.billingFrequency != 0 || product?.isBilledByUsage) {
+                result = true;
+            }
+        });
+
+        return result;
+    }
+
+    let cartIsEmpty = () => {
+        return !shoppingCart?.productQuantities?.length;
+    };
+
+    let addToCart = (productId: uint, quantity: int, isDelta: boolean = false) => {
+
+        return shoppingCartApi.changeItems({
+            shoppingCartId: shoppingCart?.id,
+            items: [
+                {
+                    productId,
+                    quantity: isDelta ? undefined : quantity,
+                    deltaQuantity: isDelta ? quantity : undefined
+                }
+            ]
+        }, includeSet).then(cart => {
+            store.set('shopping_cart_id', cart.id.toString());
+            setShoppingCart(cart);
+            return cart;
+        });
+    }
+	
+    let emptyCart = () => {
+        store.remove('shopping_cart_id');
+        setShoppingCart(null);
+        setLoading(false);
+    };
+
+    return (
+        <CartSession.Provider
+            value={{
+                shoppingCart,
+                addToCart,
+                loading,
+                emptyCart,
+                cartIsEmpty,
+                getCartQuantity,
+                hasSubscriptions
+            }}
+        >
+            {props.children}
+        </CartSession.Provider>
+    );
+};
+
+export { CartSession };
+
+export function useCart() {
+    return useContext(CartSession);
+}
