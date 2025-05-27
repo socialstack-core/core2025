de10dd15accc73d90d2f68e66fa8b7093bfb29cb
Luke Briggs <luke@kulestar.com>
2025-05-27 11:34:32 +0100

    Stripped currency tools back to roughly where it once started, fixing a wide range of bugs and inconsistencies which were well known on the NWS project

--- UI/Source/ThirdParty/Functions/CurrencyTools/CurrencyTools.js
+++ UI/Source/ThirdParty/Functions/CurrencyTools/CurrencyTools.js
@@ -1,46 +1,17 @@
 /**
- * return currency symbol for given locale
+ * return number of decimal places for given currencyCode (e.g. 2 for GBP)
  * @param {any} locale
  */
-const getCurrencySymbol = (locale, initialRender) => {
-
-	// ensure the first clientside render / SSR do the same thing (i.e. skip formatting)
-	if (initialRender || window.SERVER) {
-		// TODO: consider using currency symbol (U+00A4 / &curren;) as a placeholder
-		//return "¤";
-		return "";
-	}
-
-	var formattedValue = new Intl.NumberFormat(locale.code, {
+const fractionDigits = (currencyCode, localeCode) => {
+	return new Intl.NumberFormat(localeCode, {
 		style: 'currency',
-		currency: locale.currencyCode
-	}).format(0);
-
-	return formattedValue[0];
-}
-
-/**
- * return number of decimal places for given locale (e.g. 2 for GBP)
- * @param {any} locale
- */
-const fractionDigits = (locale, initialRender) => {
-
-	// ensure the first clientside render / SSR do the same thing (i.e. skip formatting)
-	if (initialRender || window.SERVER) {
-		// assume 2
-		return 2;
-	}
-
-	return new Intl.NumberFormat(locale.code, {
-		style: 'currency',
-		currency: locale.currencyCode,
+		currency: currencyCode,
 	}).resolvedOptions().maximumFractionDigits;
 };
 
 /**
  * format currency value
  * @param {any} value expects amount in pennies / cents etc (e.g. 12345 = 123.45)
- * @param {any} locale local locale
  * @param {any} options
  * 
  * options available are:
@@ -56,49 +27,40 @@ const fractionDigits = (locale, initialRender) => {
  * - currencyCode: defaults to locale.currencyCode
  * 
  * for example, to render an amount purchased in a foreign currency:
- * formatCurrency(12345, session.locale, { currencyCode: "DE" })
+ * formatCurrency(12345, { currencyCode: "EUR" })
  */
-const formatCurrency = (value, locale, options, initialRender) => {
+const formatCurrency = (value, options) => {
 	options = options || {};
-	var currencyFractionDigits = options.hideDecimals ? 0 : fractionDigits(locale, initialRender);
-	var hideSymbol = options.hideSymbol || (options.currencyDisplay != undefined && (options.currencyDisplay == "none" || options.currencyDisplay == false));
+	const { currencyCode, localeCode } = options;
 
-	// ensure the first clientside render / SSR do the same thing (i.e. skip formatting)
-	if (initialRender || window.SERVER) {
-		var factor = Math.pow(10, currencyFractionDigits);
-
-		if (hideSymbol) {
-			return value / factor;
-		}
-
-		var symbol = hideSymbol ? '' : (locale?.code == 'us' ? '$' : '£');
-		var major = Math.floor(value / factor);
-		var minor = value - (major * factor);
-		var result = symbol + major.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-
-		return options.hideDecimals ? result : result + '.' + minor.toString();
+	if (!currencyCode) {
+		throw new Error('currency reqd.');
 	}
 
+	var currencyFractionDigits = fractionDigits(currencyCode, localeCode);
+	var hideSymbol = options.hideSymbol || (options.currencyDisplay != undefined && (options.currencyDisplay == "none" || options.currencyDisplay == false));
+
+	var divisor = Math.pow(10, currencyFractionDigits);
+
 	if (hideSymbol) {
-		return (value / Math.pow(10, currencyFractionDigits)).toLocaleString(locale.code, {
+		return (value / divisor).toLocaleString(localeCode, {
 			minimumFractionDigits: currencyFractionDigits,
 			maximumFractionDigits: currencyFractionDigits
 		});
 
     }
 
-	return new Intl.NumberFormat(locale.code, {
+	return new Intl.NumberFormat(localeCode, {
 		style: 'currency',
-		currency: options.currencyCode ? options.currencyCode : locale.currencyCode,
+		currency: currencyCode,
 		currencyDisplay: !hideSymbol && options.currencyDisplay ? options.currencyDisplay : undefined,
 		minimumFractionDigits: options.hideDecimals ? 0 : undefined,
 		maximumFractionDigits: options.hideDecimals ? 0 : undefined
-	}).format(value / Math.pow(10, fractionDigits(locale)));
+	}).format(value / divisor);
 
 };
 
 export {
-	getCurrencySymbol,
 	fractionDigits,
 	formatCurrency
 };
