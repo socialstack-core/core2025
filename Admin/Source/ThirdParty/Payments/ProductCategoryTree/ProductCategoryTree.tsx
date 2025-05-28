a36e58c3e692d7d3fe22661cde88ce47cc6d4b23
Luke Briggs <luke@kulestar.com>
2025-05-28 11:46:43 +0100

    Products tree now uses the breadcrumb bar

--- Admin/Source/ThirdParty/Payments/ProductCategoryTree/ProductCategoryTree.tsx
+++ Admin/Source/ThirdParty/Payments/ProductCategoryTree/ProductCategoryTree.tsx
@@ -1,5 +1,6 @@
-import TreeView from 'Admin/TreeView';
+import TreeView, { buildBreadcrumbs } from 'Admin/TreeView';
 import SubHeader from 'Admin/SubHeader';
+import { useRouter } from 'UI/Router';
 import Default from 'Admin/Templates/BaseAdminTemplate';
 import { useState, useEffect } from 'react';
 import productCategoryApi, { ProductCategory } from 'Api/ProductCategory';
@@ -7,14 +8,20 @@ import productCategoryApi, { ProductCategory } from 'Api/ProductCategory';
 export default function ProductCategoryTree(props) {
 	var addProductUrl = '/en-admin/product/add';
 	var addCategoryUrl = '/en-admin/productcategory/add';
+	const { pageState } = useRouter();
+	const { query } = pageState;
+	var path = query?.get("path") || "";
+
+	var breadcrumbs = buildBreadcrumbs(
+		'/en-admin/product',
+		`Products`,
+		path,
+		'/en-admin/product'
+	);
 
 	return (
 		<Default>
-			<SubHeader title={`Edit Products`} breadcrumbs={[
-				{
-					title: `Products`
-				}
-			]} />
+			<SubHeader title={`Edit Products`} breadcrumbs={breadcrumbs} />
 			<div className="sitemap__wrapper">
 				<div className="sitemap__internal">
 					<TreeView onLoadData={(path) => {
