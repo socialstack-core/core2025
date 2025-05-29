d546909e7fdd288e531bb7c49d4cd0053782634a
Conan Ablewhite <conan.ablewhite@4-roads.com>
2025-05-29 01:39:04 +0100

    header / footer components updated (WIP)

--- UI/Source/Footer/Footer.tsx
+++ UI/Source/Footer/Footer.tsx
@@ -1,17 +1,212 @@
+import Html from 'UI/Html';
+import { getContactLink } from 'UI/Functions/ContactTools';
+import { useState, useEffect } from "react";
+
 /**
  * Props for the Footer component.
  */
 interface FooterProps {
 
+	/** 
+	 * contact number (e.g. "0123 456 7890")
+	 */
+	contactNumber?: string,
+
+	/** 
+	 * contact link (e.g. "/contact-us")
+	 */
+	contactLink?: string,
+
+	/** 
+	 * address (use embedded <br/> to split lines)
+	 */
+	address?: string,
+
+	/** 
+	 * copyright year from (defaults to current year)
+	 */
+	copyrightFrom?: int,
+
+	/** 
+	 * copyright year to (defaults to current year)
+	 */
+	copyrightTo?: int,
+
+	/** 
+	 * copyright name (prefixed with (C) symbol and year(s))
+	 */
+	copyright?: string
+}
+
+/**
+ * Props for each footer link.
+ */
+interface FooterLinkProps {
+
+	/** 
+	 * label
+	 */
+	label: string,
+
+	/** 
+	 * URL
+	 */
+	url: string
 }
 
 /**
  * The Footer React component.
  * @param props React props.
  */
-const Footer: React.FC<FooterProps> = (props) => {
-	return <div className="footer">
-		Hello, footer world!
+const Footer: React.FC<FooterProps> = ({ contactNumber, contactLink, address, copyrightFrom, copyrightTo, copyright, ...props }) => {
+
+	if (!contactNumber) {
+		contactNumber = "0808 189 2044";
+	}
+
+	const contactHref = getContactLink(contactNumber, contactLink);
+	const [column2Links, setColumn2Links] = useState<FooterLinkProps[]>([]);
+	const [column3Links, setColumn3Links] = useState<FooterLinkProps[]>([]);
+
+	useEffect(() => {
+		// TODO: retrieve footer links from DB
+		setColumn2Links([
+			{
+				label: `About`,
+				url: `/about`
+			},
+			{
+				label: `What We Offer`,
+				url: `/what-we-offer`
+			},
+			{
+				label: `All products`,
+				url: `/products`
+			},
+			{
+				label: `Testimonials`,
+				url: `/testimonials`
+			},
+			{
+				label: `Contact us`,
+				url: `/contact-us`
+			},
+		]);
+
+		setColumn3Links([
+			{
+				label: `Privacy Policy`,
+				url: `/privacy-policy`
+			},
+			{
+				label: `Cyber Security Certificate`,
+				url: `/cyber-security-certificate`
+			},
+			{
+				label: `Terms & Conditions`,
+				url: `/terms-and-conditions`
+			},
+			{
+				label: `Delivery Information`,
+				url: `/delivery`
+			},
+			{
+				label: `Returns Policy`,
+				url: `/returns`
+			},
+		]);
+
+	}, [])
+
+	// calculate copyright period, either:
+	// - only from year supplied = [from]-[currentYear]
+	// - from and to years supplied = [from]-[to]
+	// - only to year supplied = [to]
+	// - neither from or to year supplied = [currentYear]
+	const currentYear: number = new Date().getFullYear();
+	let fromYear: number = validateYear(copyrightFrom);
+	let toYear: number = validateYear(copyrightTo);
+
+	// ignore invalid date ranges
+	if (toYear <= fromYear) {
+		toYear = 0;
+	}
+
+	let copyrightYear = currentYear.toString();
+
+	if (fromYear && !toYear) {
+		copyrightYear = fromYear >= currentYear ? fromYear.toString() : `${fromYear}&mdash;${currentYear}`;
+	}
+
+	if (!fromYear && toYear) {
+		copyrightYear = toYear.toString();
+	}
+
+	if (fromYear && toYear) {
+		copyrightYear = `${fromYear}&mdash;${toYear}`;
+	}
+
+	function validateYear(year: number): number {
+
+		if (isNaN(year) || year < 1900) {
+			return 0;
+		}
+
+		return year;
+	}
+
+	return <div className="site-footer">
+		<div class="site-footer__internal">
+
+			<div class="site-footer__contact">
+				{contactHref && <>
+					<a href={contactHref} class="site-footer__contact-link">
+						{contactHref.startsWith("tel:") && <>
+							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 45 44" fill="none">
+								<path d="M41.24 27.9l-5.92-2.94a5.45 5.45 0 00-7.26 2.27c-1.87-.73-5-2.92-6.82-4.68-1.76-1.83-3.92-4.89-4.68-6.76a5.46 5.46 0 002.39-7.31l-2.9-5.81C14.38-.2 9.8-.3 7.84.28c-2.8.83-5.23 2.8-6.66 5.41-1.9 3.5-1.14 7.74-.13 11.95a37.69 37.69 0 009.98 15.27 37.94 37.94 0 0015.49 10.1c2.22.53 4.51.99 6.71.99 1.8 0 3.52-.31 5.1-1.17a11.26 11.26 0 005.4-6.66c.58-1.98.48-6.56-2.49-8.26zm-1.04 7.25a7.5 7.5 0 01-3.65 4.45c-2.37 1.28-5.85.62-9.04-.13-5.08-1.7-9.74-4.76-13.92-9.16-4.32-4.1-7.39-8.76-9.05-13.68-.8-3.35-1.47-6.83-.19-9.2A7.62 7.62 0 018.81 3.8c.31-.1.73-.13 1.19-.13 1.14 0 2.51.27 2.77.73l2.84 5.7c.22.46.26.95.1 1.43-.17.48-.52.84-.98 1.05L13.7 13c-.7.29-1.14.95-1.14 1.7.02 3.5 4.73 9.1 6.03 10.43 1.3 1.26 6.88 5.96 10.4 6 .7 0 1.34-.4 1.65-1.03 0 0 .24-.5.57-1.12a1.82 1.82 0 012.42-.79l5.83 2.9c.75.44 1 2.95.7 4.01l.05.04z" fill="currentColor" />
+							</svg>
+						</>}
+						{contactNumber?.length > 0 ? contactNumber : contactLink}
+					</a>
+				</>}
+
+				{address?.length > 0 && <>
+					<Html tag="address">
+						{address}
+					</Html>
+				</>}
+
+				{copyright?.length > 0 && <>
+					&copy; {copyrightYear} {copyright}
+				</>}
+
+			</div>
+
+			{column2Links?.length > 0 && <>
+				<menu class="site-footer__column2">
+					{column2Links.map(link => {
+						return <li>
+							<a href={link.url}>
+								{link.label}
+							</a>
+						</li>;
+					})}
+				</menu>
+			</>}
+
+			{column3Links?.length > 0 && <>
+				<menu class="site-footer__column3">
+					{column3Links.map(link => {
+						return <li>
+							<a href={link.url}>
+								{link.label}
+							</a>
+						</li>;
+					})}
+				</menu>
+			</>}
+
+		</div>
 	</div>;
 }
 
--- UI/Source/Header/Header.tsx
+++ UI/Source/Header/Header.tsx
@@ -1,24 +1,248 @@
 import Image from 'UI/Image';
+import Html from 'UI/Html';
+import NotificationBadge from 'UI/NotificationBadge';
+import { useSession } from 'UI/Session';
+import exampleLogoRef from './example-logo.png';
+import { getContactLink } from 'UI/Functions/ContactTools';
+import { useState, useEffect } from "react";
 
 /**
  * Props for the Header component.
  */
 interface HeaderProps {
+
+	/** 
+	 * contact number (e.g. "0123 456 7890")
+	 */
+	contactNumber?: string,
+
 	/**
 	 * The website logo.
 	 */
-	logoRef?: FileRef
+	logoRef?: FileRef,
+
+	/** 
+	 * optional message to be displayed next to contact number
+	 */
+	message?: string,
+
+	/** 
+	 * optional placeholder text for search field
+	 */
+	searchPlaceholder?: string,
+
+	/**
+	 * outstanding notifications count
+	 */
+	notificationCount: number
+}
+
+/**
+ * Props for each header link.
+ */
+interface HeaderLinkProps {
+
+	/** 
+	 * label
+	 */
+	label: string,
+
+	/** 
+	 * URL
+	 */
+	url: string
 }
 
 /**
  * The website header React component.
  * @param props React props.
  */
-const Header: React.FC<HeaderProps> = (props) => {
+const Header: React.FC<HeaderProps> = ({ contactNumber, logoRef, message, searchPlaceholder, notificationCount, ...props }) => {
+	if (!contactNumber) {
+		contactNumber = "0808 189 2044";
+	}
+
+	const contactHref = getContactLink(contactNumber);
+
+	const [primaryLinks, setPrimaryLinks] = useState<HeaderLinkProps[]>([]);
+	const { session } = useSession();
+	var { user } = session;
+
+	const notificationBellSVG = <>
+		<svg viewBox="0 0 12 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<path d="M4.67333 14.1116C4.67333 14.8791 5.26667 15.5 6 15.5C6.73333 15.5 7.32667 14.8791 7.32667 14.1116H4.67333ZM6 3.63953C7.84 3.63953 9.33333 5.20233 9.33333 7.12791V12.0116H2.66667V7.12791C2.66667 5.20233 4.16 3.63953 6 3.63953ZM6 0.5C5.44667 0.5 5 0.967442 5 1.54651V2.36279C2.90667 2.83721 1.33333 4.7907 1.33333 7.12791V11.314L0 12.7093V13.407H12V12.7093L10.6667 11.314V7.12791C10.6667 4.7907 9.09333 2.83721 7 2.36279V1.54651C7 0.967442 6.55333 0.5 6 0.5ZM5.33333 5.03488H6.66667V7.82558H5.33333V5.03488ZM5.33333 9.22093H6.66667V10.6163H5.33333V9.22093Z" fill="currentColor" />
+		</svg>
+	</>;
+
+	useEffect(() => {
+		// TODO: retrieve primary links from DB
+		setPrimaryLinks([
+			{
+				label: `Shop for products`,
+				url: `/products`
+			},
+			{
+				label: `Contact Us`,
+				url: `/contact-us`
+			},
+			{
+				label: `Delivery Information`,
+				url: `/delivery`
+			},
+			{
+				label: `About Us`,
+				url: `/about-us`
+			},
+		]);
+
+	}, [])
+
+	if (!logoRef) {
+		logoRef = exampleLogoRef;
+	}
+
+	if (!message || !message.length) {
+		message = `Free UK Delivery Over &pound;50`;
+	}
+
+	if (!searchPlaceholder || !searchPlaceholder.length) {
+		searchPlaceholder = `Search for a product name, category or code`
+	}
+
+	if (!notificationCount) {
+		notificationCount = 0;
+	}
+
+	// TODO
+	var basketCount = 2;
+	var basketTotal = 221.87;
+
+	let GBPound = new Intl.NumberFormat('en-GB', {
+		style: 'currency',
+		currency: 'GBP',
+	});
+
+	const showContact = contactHref?.length || message?.length;
+
 	return (
-		<div className="header">
-			Hello, header world! 
-			<Image fileRef={props.logoRef} />
+		<div className="site-header">
+			<div className="site-header__internal">
+				<a href="/" className="site-header__logo">
+					<Image fileRef={logoRef} />
+					{/*
+					<img src="https://assets.codepen.io/5477427/acticare_logo.png" alt="" />
+					*/}
+				</a>
+
+				<div className="site-header__search">
+					<input type="search" placeholder={searchPlaceholder} />
+					<div className="site-header__search-dropdown">
+					...
+						{/*
+						<h2>
+							Recent searches
+							<a href="#">
+								1
+							</a>
+							<a href="#">
+								2
+							</a>
+							<a href="#">
+								3
+							</a>
+						</h2>
+							*/}
+					</div>
+				</div>
+
+				<div className="site-header__actions">
+
+					{/* sign in link */}
+					{!user && <>
+						<a href="/en-admin" className="btn btn-primary site-header__actions-login">
+							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 28 36" fill="none">
+								<path d="M22.5106 9C22.5106 4.035 18.4756 0 13.5106 0C8.54556 0 4.51056 4.035 4.51056 9C4.51056 13.965 8.54556 18 13.5106 18C18.4756 18 22.5106 13.965 22.5106 9ZM7.51056 9C7.51056 5.685 10.1956 3 13.5106 3C16.8256 3 19.5106 5.685 19.5106 9C19.5106 12.315 16.8256 15 13.5106 15C10.1956 15 7.51056 12.315 7.51056 9Z" fill="currentColor" />
+								<path d="M0.0105591 33V34.5C0.0105591 35.325 0.685559 36 1.51056 36C2.33556 36 3.01056 35.325 3.01056 34.5V33C3.01056 28.035 7.04556 24 12.0106 24H15.0106C19.9756 24 24.0106 28.035 24.0106 33V34.5C24.0106 35.325 24.6856 36 25.5106 36C26.3356 36 27.0106 35.325 27.0106 34.5V33C27.0106 26.385 21.6256 21 15.0106 21H12.0106C5.39556 21 0.0105591 26.385 0.0105591 33Z" fill="currentColor" />
+							</svg>
+							{`Sign in`}
+						</a>
+					</>}
+
+					{/* account */}
+					{user && <>
+						<button type="button" className="btn btn-primary site-header__actions-account">
+							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 28 36" fill="none">
+								<path d="M22.5106 9C22.5106 4.035 18.4756 0 13.5106 0C8.54556 0 4.51056 4.035 4.51056 9C4.51056 13.965 8.54556 18 13.5106 18C18.4756 18 22.5106 13.965 22.5106 9ZM7.51056 9C7.51056 5.685 10.1956 3 13.5106 3C16.8256 3 19.5106 5.685 19.5106 9C19.5106 12.315 16.8256 15 13.5106 15C10.1956 15 7.51056 12.315 7.51056 9Z" fill="currentColor" />
+								<path d="M0.0105591 33V34.5C0.0105591 35.325 0.685559 36 1.51056 36C2.33556 36 3.01056 35.325 3.01056 34.5V33C3.01056 28.035 7.04556 24 12.0106 24H15.0106C19.9756 24 24.0106 28.035 24.0106 33V34.5C24.0106 35.325 24.6856 36 25.5106 36C26.3356 36 27.0106 35.325 27.0106 34.5V33C27.0106 26.385 21.6256 21 15.0106 21H12.0106C5.39556 21 0.0105591 26.385 0.0105591 33Z" fill="currentColor" />
+							</svg>
+							Account
+							{notificationCount > 0 && <>
+								<NotificationBadge icon={notificationBellSVG} />
+							</>}
+						</button>
+					</>}
+
+					{/* toggle basket */}
+					<button type="button" className="btn btn-outline-primary site-header__actions-basket">
+						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 54 53" fill="none">
+							<path d="M1.96524 4.44736C5.36241 4.84443 6.6198 5.3518 8.34045 7.07242C9.99492 9.16806 15.0245 27.4994 15.4657 30.5877L14.6716 33.7201C14.1642 35.7055 14.6054 37.779 15.8848 39.3894C17.1422 40.9997 19.0393 41.9262 21.0909 41.9262H44.1211C45.3344 41.9262 46.327 40.9335 46.327 39.7203C46.327 38.507 45.3344 37.5143 44.1211 37.5143H21.0909C20.407 37.5143 19.7673 37.2055 19.3482 36.6761C18.929 36.1467 18.7746 35.4408 18.9511 34.779L19.3702 33.1025H36.4002C41.9372 33.1025 46.9447 29.595 48.8418 24.389L52.5699 14.1755C53.3861 11.9475 53.0552 9.47689 51.6875 7.53567C50.3198 5.59445 48.1139 4.44736 45.7314 4.44736H17.6496C16.4363 4.44736 15.4436 5.44003 15.4436 6.6533C15.4436 7.86656 16.4363 8.85923 17.6496 8.85923H45.7314C46.68 8.85923 47.5182 9.30042 48.0697 10.0725C48.6212 10.8446 48.7315 11.7931 48.4227 12.6755L44.6946 22.889C43.4372 26.3744 40.1062 28.6906 36.4002 28.6906H19.5467C18.3776 21.3448 12.7965 5.26356 11.495 3.96206C8.71546 1.18258 6.28891 0.454619 2.4726 0.0134315C1.25933 -0.118925 0.178408 0.74139 0.0239907 1.95465C-0.108367 3.16792 0.751959 4.24883 1.96524 4.40324V4.44736Z" fill="currentColor" />
+							<path d="M18.7526 53C20.58 53 22.0615 51.5186 22.0615 49.6911C22.0615 47.8636 20.58 46.3822 18.7526 46.3822C16.9251 46.3822 15.4436 47.8636 15.4436 49.6911C15.4436 51.5186 16.9251 53 18.7526 53Z" fill="currentColor" />
+							<path d="M43.018 53C44.8455 53 46.327 51.5186 46.327 49.6911C46.327 47.8636 44.8455 46.3822 43.018 46.3822C41.1906 46.3822 39.7091 47.8636 39.7091 49.6911C39.7091 51.5186 41.1906 53 43.018 53Z" fill="currentColor" />
+						</svg>
+						{basketCount == 0 && <>
+							{`Basket`}
+						</>}
+						{GBPound.format(basketTotal)}
+						<NotificationBadge count={basketCount} />
+					</button>
+
+					{/* VAT switch */}
+					<div className="site-header__actions-vat">
+						<div className="form-check form-switch">
+							<input className="form-check-input" type="checkbox" role="switch" id="switchVAT" />
+							<label className="form-check-label" htmlFor="switchVAT">
+								{`Exc VAT`}
+							</label>
+						</div>
+					</div>
+
+				</div>
+
+				{/* primary nav */}
+				{primaryLinks?.length > 0 && <>
+					<menu class="site-header__nav">
+						{primaryLinks.map(link => {
+							return <li>
+								<a href={link.url}>
+									{link.label}
+								</a>
+							</li>;
+						})}
+					</menu>
+				</>}
+
+				{showContact && <>
+					<div className="site-header__contact">
+
+						{contactHref && <>
+							<a href={contactHref} className="site-header__tel">
+								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 45 44" fill="none">
+									<path d="M41.2431 27.9068L35.3209 24.9558C32.6623 23.6178 29.4903 24.6259 28.0602 27.2287C26.19 26.4955 23.0547 24.3143 21.2395 22.5547C19.4793 20.7218 17.3158 17.6608 16.564 15.7912C17.7925 15.1496 18.7276 14.1049 19.2043 12.7852C19.7177 11.3738 19.626 9.8158 18.9476 8.47776L16.0506 2.66736C14.3821 -0.192015 9.79834 -0.301992 7.83648 0.284547C5.0312 1.10937 2.61097 3.07061 1.18083 5.6917C-0.726028 9.1926 0.044049 13.4267 1.05248 17.6424C2.95934 23.2879 6.333 28.4567 11.0268 32.9108C15.5556 37.6764 20.7078 41.049 26.52 43.0102C28.7386 43.5418 31.0304 44 33.2307 44C35.0275 44 36.751 43.6884 38.3278 42.8269C40.9498 41.3972 42.9116 38.9778 43.7367 36.1734C44.3051 34.1938 44.2134 29.6115 41.2431 27.9068ZM40.198 35.1469C39.648 37.0165 38.3278 38.6478 36.5493 39.601C34.1841 40.884 30.7004 40.2242 27.5101 39.4727C22.4313 37.768 17.7741 34.707 13.5937 30.308C9.26663 26.2022 6.20465 21.5466 4.53616 16.6343C3.74774 13.28 3.06935 9.79747 4.35281 7.43299C5.32457 5.67337 6.93806 4.33533 8.80825 3.78545C9.11995 3.6938 9.54165 3.65714 10 3.65714C11.1368 3.65714 12.5119 3.93208 12.7686 4.39032L15.6106 10.0907C15.8306 10.549 15.8673 11.0439 15.7023 11.5204C15.5372 11.997 15.1889 12.3636 14.7305 12.5652C14.1438 12.8218 13.7037 13.0051 13.6854 13.0051C12.9887 13.2984 12.5486 13.9582 12.5486 14.7097C12.567 18.2106 17.2791 23.8011 18.5809 25.1391C19.8827 26.4038 25.4566 31.0962 28.9769 31.1328C29.6736 31.1328 30.3154 30.7479 30.6271 30.1064C30.6271 30.1064 30.8654 29.6115 31.1955 28.9883C31.6722 28.1085 32.7356 27.7602 33.6157 28.2001L39.4463 31.0961C40.198 31.5361 40.4547 34.0472 40.143 35.1103L40.198 35.1469Z" fill="currentColor" />
+								</svg>
+								{contactNumber}
+							</a>
+						</>}
+
+						{message?.length > 0 && <>
+							<Html className="site-header__message">
+								{message}
+							</Html>
+						</>}
+
+					</div>
+				</>}
+
+			</div>
 		</div>
 	);
 }
