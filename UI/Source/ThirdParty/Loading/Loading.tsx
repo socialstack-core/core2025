cec54e35830234edb39054db3b060cb2b2a5b903
Conan Ablewhite <conan.ablewhite@4-roads.com>
2025-06-01 21:53:00 +0100

    UI/Loading small option added

--- UI/Source/ThirdParty/Loading/Loading.tsx
+++ UI/Source/ThirdParty/Loading/Loading.tsx
@@ -3,6 +3,11 @@
  */
 interface LoadingProps extends React.HTMLAttributes<HTMLSpanElement> {
 	/**
+	 * Render small version of loading spinner.
+	 */
+	small?: boolean,
+
+	/**
 	 * Optionally change the message being displayed.
 	 */
 	message?: string
@@ -15,7 +20,7 @@ const Loading: React.FC<LoadingProps> = (props) => {
 	let message = props.message || `Loading ...`;
 	
 	return (
-		<div className="loading">
+		<div className={props.small ? "loading loading--small" : "loading"}>
 			<svg className="loading__spinner" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
 				<circle fill="none" stroke-width="6" stroke-linecap="round" cx="33" cy="33" r="30"></circle>
 			</svg>
