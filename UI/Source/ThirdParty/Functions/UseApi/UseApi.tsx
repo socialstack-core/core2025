31c5e867bd1bf3767ba9419cd675137b6cecd477
Conan Ablewhite <conan.ablewhite@4-roads.com>
2025-07-02 13:10:27 +0100

    UseApi update (for John)

--- UI/Source/ThirdParty/Functions/UseApi/UseApi.tsx
+++ UI/Source/ThirdParty/Functions/UseApi/UseApi.tsx
@@ -3,23 +3,25 @@ import { useEffect, useState } from 'react';
 
 const useApi = <T,>(loader: () => Promise<T>, deps?: React.DependencyList) => {
     const state = useState<T>(() => {
-        // SSR hydration logic placeholder
+        // Todo: attempt to obtain data from SSR content response
         return null as T;
     });
 
     const [current, setCurrent] = state;
 
     useEffect(() => {
-        let isCurrent = true;
-
-        loader().then(val => {
-            if (!val || !isCurrent) return;
-            setCurrent(val);
+        loader()
+            .then(val => {
+                if (!val) {
+                    return;
+                }
+                setCurrent(val)
         });
 
-        return () => {
-            isCurrent = false;
-        };
+    // [Lint disabled here] 
+    // Reason: the dependency array expects an array literal, we're
+    // passing dependencies here from the method which
+    // renders this impossible.
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, deps);
 
