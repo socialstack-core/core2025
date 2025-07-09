0a5a5191939912098030879b60be7ae18f0401de
Conan Ablewhite <conan.ablewhite@4-roads.com>
2025-07-09 14:45:52 +0100

    checkout address / delivery estimate update

--- UI/Source/ThirdParty/Payments/Checkout/DeliveryEstimates.tsx
+++ UI/Source/ThirdParty/Payments/Checkout/DeliveryEstimates.tsx
@@ -36,76 +36,72 @@ const DeliveryEstimates: React.FC<DeliveryEstimatesProps> = (props) => {
 			// For example, you can choose to get things as soon as possible with 5/6 items showing up tomorrow
 			// and 1/6 the day after. Or you can choose to get all 6 the day after.
 
-			//xselectedValue={selectedAddress?.id || -1}
-			//selectedValue={selectedAddress}
+			return <Input type="radio" name={"delivery"} value={estimate.id.toString()} checked={value?.id == estimate.id} noWrapper
+				onChange={() => setValue(estimate)}
+				label={estimateDetail.deliveries.map(deliveryInfo => {
+					// DeliveryInfo C# type, in DeliveryEstimate.cs
+					let dateDescription: String;
 
-			return estimateDetail.deliveries.map(deliveryInfo => {
-				let dateDescription: String;
-				// DeliveryInfo C# type, in DeliveryEstimate.cs
+					// If there are >1 deliveries, then deliveryInfo.Products is
+					// set indicating what is present in *this* proposed delivery.
 
-				// If there are >1 deliveries, then deliveryInfo.Products is
-				// set indicating what is present in *this* proposed delivery.
-
-				if (!deliveryInfo.slotStartUtc) {
-					// A date isn't known for this service.
-					// It happens on JIT style delivery where the product is ordered from an upstream usually foreign supplier.
-					// "We'll let you know when a date is confirmed" etc.
-					dateDescription = `Date to be confirmed`;
-				} else {
-					var date = new Date(deliveryInfo.slotStartUtc);
-
-					if (deliveryInfo.timeWindowLength) {
-						// It's a time slot range. Date is the start of the slot.
-						var slotEnd = addMinutes(date, deliveryInfo.timeWindowLength);
-
-						let from = new Intl.DateTimeFormat('en-GB', {
-							year: 'numeric',
-							month: 'long',
-							day: 'numeric',
-							hour: '2-digit',
-							minute: '2-digit'
-						}).format(date);
-
-						let to = new Intl.DateTimeFormat('en-GB', {
-							year: 'numeric',
-							month: 'long',
-							day: 'numeric',
-							hour: '2-digit',
-							minute: '2-digit'
-						}).format(slotEnd);
-
-						// The time slot is between date and slotEnd.
-						dateDescription = `${from} - ${to}`;
+					if (!deliveryInfo.slotStartUtc) {
+						// A date isn't known for this service.
+						// It happens on JIT style delivery where the product is ordered from an upstream usually foreign supplier.
+						// "We'll let you know when a date is confirmed" etc.
+						dateDescription = `Date to be confirmed`;
 					} else {
-						// Only use the day component. Time must be ignored.
-						dateDescription = `${new Intl.DateTimeFormat('en-GB', {
-							year: 'numeric',
-							month: 'long',
-							day: 'numeric'
-						}).format(date)}`;
+						var date = new Date(deliveryInfo.slotStartUtc);
+
+						if (deliveryInfo.timeWindowLength) {
+							// It's a time slot range. Date is the start of the slot.
+							var slotEnd = addMinutes(date, deliveryInfo.timeWindowLength);
+
+							let from = new Intl.DateTimeFormat('en-GB', {
+								year: 'numeric',
+								month: 'long',
+								day: 'numeric',
+								hour: '2-digit',
+								minute: '2-digit'
+							}).format(date);
+
+							let to = new Intl.DateTimeFormat('en-GB', {
+								year: 'numeric',
+								month: 'long',
+								day: 'numeric',
+								hour: '2-digit',
+								minute: '2-digit'
+							}).format(slotEnd);
+
+							// The time slot is between date and slotEnd.
+							dateDescription = `${from} - ${to}`;
+						} else {
+							// Only use the day component. Time must be ignored.
+							dateDescription = `${new Intl.DateTimeFormat('en-GB', {
+								year: 'numeric',
+								month: 'long',
+								day: 'numeric'
+							}).format(date)}`;
+						}
 					}
-				}
 
-				return <Input type="radio" name={"delivery"} value={estimate.id.toString()} checked={value?.id == estimate.id} noWrapper
-					onChange={() => setValue(estimate)}
-					label={<>
-						<div className="payment-checkout__delivery-estimate">
-							<h3 className="payment-checkout__delivery-estimate__date">
-								{dateDescription}
-							</h3>
-							<strong className="payment-checkout__delivery-estimate__name">
-								{deliveryInfo.deliveryName}
-							</strong>
-							<p className="payment-checkout__delivery-estimate__notes">
-								{deliveryInfo.deliveryNotes}
-							</p>
-							<p className="payment-checkout__delivery-estimate__price">
-								{formatCurrency(estimateDetail.price, { currencyCode: estimateDetail.currency })}
-							</p>
-						</div>
-					</>} />
-
-			});
+					return <div className="payment-checkout__delivery-estimate">
+						{/* temporarily hidden
+						<h3 className="payment-checkout__delivery-estimate__date">
+							{dateDescription}
+						</h3>
+						*/}
+						<strong className="payment-checkout__delivery-estimate__name">
+							{deliveryInfo.deliveryName}
+						</strong>
+						<p className="payment-checkout__delivery-estimate__notes">
+							{deliveryInfo.deliveryNotes}
+						</p>
+						<p className="payment-checkout__delivery-estimate__price">
+							{formatCurrency(estimateDetail.price, { currencyCode: estimateDetail.currency })}
+						</p>
+					</div>
+				})} />;
 
 		})}
 	</div>;
