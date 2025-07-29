edbb50a6b2f1b816ff41fddb5f87a11c35b12e2d
hudson1998x <jhudsonjejames@gmail.com>
2025-07-29 12:53:05 +0100

    Added cleanup utility for orphaned attribute values.

--- Api/ThirdParty/Payments/ProductAttributeValueService.cs
+++ Api/ThirdParty/Payments/ProductAttributeValueService.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Threading.Tasks;
 using Api.CanvasRenderer;
 using Api.Contexts;
@@ -43,9 +42,40 @@ namespace Api.Payments
 			Events.ProductAttributeValue.BeforeCreate.AddEventListener(async (ctx, attrValue) => await ValidateAttributeValue(ctx, attrValue));
 			Events.ProductAttributeValue.BeforeUpdate.AddEventListener(ValidateAttributeValue);
 			
+			// added to prevent orphaned values from showing and being searchable
+			// in the Admin Panel, this doesn't delete attribute values from the products themselves,
+			// but 40m is useless without knowing its attribute. 
+			Events.ProductAttribute.BeforeDelete.AddEventListener(async (ctx, attribute) =>
+			{
+				// This deletes all attribute values for a specified
+				// attribute, this doesn't clean up existing orphans
+				// that will happen in a separate function.
+				await DeleteAttributeValues(ctx, attribute);
+				return attribute;
+			});
+			
 			Cache();
 		}
+		
+		/// <summary>
+		/// Removes a product attribute's values when an
+		/// attribute is deleted. 
+		/// </summary>
+		/// <param name="context"></param>
+		/// <param name="attribute"></param>
+		/// <returns></returns>
+		public async ValueTask DeleteAttributeValues(Context context, ProductAttribute attribute)
+		{
+			var values = await Where("ProductAttributeId = ?", DataOptions.IgnorePermissions)
+				.Bind(attribute.Id)
+				.ListAll(context);
 
+			foreach (var value in values)
+			{
+				await Delete(context, value);
+			}
+		}
+		
 		private ValueTask<ProductAttributeValue> ValidateAttributeValue(Context context, ProductAttributeValue attrValue, ProductAttributeValue original = null)
 		{
 
